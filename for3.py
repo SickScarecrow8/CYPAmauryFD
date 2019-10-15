23f540e14e32160383fe7c8d599d254bb8bcd177
SickScarecrow8 <amaury.f.d8@gmail.com>
2019-10-15 17:40:21 -0500

    Ejemplos de la clase

---
+++ for3.py
@@ -0,0 +1,10 @@
+for V in range(3,31,3):
+    print("Hola. ",V)
+for V in range(20,-1,-1):
+    print("Hola. ",V)
+print('---------------------------')
+n = int(input("Dame un numero entero positivo: "))
+for X in range(0,n,1):
+    print("+",end="")
+
+
---
+++ libro/ejemplo3_2.py
@@ -0,0 +1,6 @@
+NOMINA = 0
+for I in range(1,11,1):
+    SUE = float ( input ( "Ingresa el sueldo del empleado: "))
+    NOMINA += SUE # NOMINA = NOMINA + SUE
+print("La nomina de la empresa es: ", NOMINA)
+
