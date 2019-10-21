788fd3279d65ffbf2a480ecfb117865bdfe1a916
SickScarecrow8 <amaury.f.d8@gmail.com>
2019-10-21 02:08:25 -0500

    Todos los programas funcionan.

---
+++ libro/problemas_resueltos/problema3_4.py
@@ -0,0 +1,13 @@
+NOM = 0
+SUE = float(input("Ingrese el sueldo del empleado: "))
+while(SUE != -1):
+    if SUE < 1000:
+        NSUE = SUE * 1.15
+    else:
+        NSUE = SUE * 1.12
+    NOM += NSUE
+    print(f"El nuevo sueldo es de {NSUE}.")
+    SUE = float(input("Ingrese el sueldo del empleado: "))
+print(f"La nomina es de {NOM}.")
+print("Fin del programa.")
+
---
+++ libro/problemas_resueltos/problema3_9.py
@@ -0,0 +1,9 @@
+SERIE = 0
+N = int(input("Ingresa el ultimo valor de la serie: "))
+I = 1
+for I in range(1,N + 1,I):
+    SERIE = SERIE + (I ** I)
+    I += 1
+print(SERIE)
+print("Fin del programa.")
+
